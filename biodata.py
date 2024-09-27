bfa90b76fd0c4dba7194ee6e1fce4343d3a2db36
falah0412 <nurfalahhidayatun35@gmail.com>
2024-09-27 10:53:44 +0700

    Add files via upload

---
+++ biodata.py
@@ -0,0 +1,12 @@
+nama_depan = "hidayatun"
+nama_belakang = 'nurfalah'
+nama_lengkap = nama_depan + '' + nama_belakang
+usia = '18'
+alamat = "mangkol"
+kata_mutiara = "jika hanya berdiam diri maka tidak akan ada kemajuan"
+
+print (nama_lengkap, '('+ usia +')',',','dari', alamat,',kata mutiara:', kata_mutiara)
+
+print('\ntipe dari nama_lengkap:', type(nama_lengkap), 'tipe dari usia:',type (usia))
+print('tipe dari alamat:', type(alamat))
+print('tipe dari kata_mutiara:', type(kata_mutiara))
