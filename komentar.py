646851cc2899e2ea8a0a2a0356087e5758b3b992
falah0412 <nurfalahhidayatun35@gmail.com>
2024-09-27 10:20:58 +0700

    Add files via upload

---
+++ komentar.py
@@ -0,0 +1,9 @@
+#variabel a merepresentasikan panjang
+a = 5
+b = 10 #variabel b merepresentasikan tinggi
+
+"""
+    dan variabel c mempresentasikan luas persegi dari hasil 
+    perkalia variabel a dan variabel c 
+"""
+c = a*b
---
+++ variabel.py
@@ -0,0 +1,7 @@
+nama ='hidayatun nurfalah'
+usia =18
+belum_menikah = True
+
+print('nama:',nama)
+print('usia:',usia)
+print('belum_menikah:',belum_menikah)
