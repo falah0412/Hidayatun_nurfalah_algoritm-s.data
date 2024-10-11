c1c210c8acf7c1b0c54074978364cec4a8b86492
falah0412 <nurfalahhidayatun35@gmail.com>
2024-10-11 09:10:19 +0700

    Add files via upload

---
+++ op_keanggotaan.py
@@ -0,0 +1,24 @@
+perusahaan = 'microsoft'
+list_pulau = ['jawa','sumatra','sulawesi']
+mahasiswa = {
+    'nama' : 'hidayatun nurfalah',
+    'asal' : 'lubuk besar'
+}
+
+print(
+    "apakah 'c' ada di variabel perusahaan?",
+    'c' in perusahaan 
+)
+print (
+    "apakah 'z' tidak ada di variabel perusahaan?",
+    'c' not in perusahaan
+)
+print(
+    "apakah 'madura' tidak ada di variabel list_pulau?",
+    'madura' not in perusahaan
+)
+
+print(
+    "apakah atribut 'nama' tidak ada di variabel mahasiswa?",
+    'nama' in perusahaan
+)
